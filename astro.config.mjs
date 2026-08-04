b347a8a8983ca7c54231d82c4674207f175d11c1
Tharaka <tharaka.malwaththa@gmail.com>
2026-08-03 21:45:17 -0400

    modified: base link

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://Thaaaraka97.github.io',
-  output: '/portfolio',
+  base: '/portfolio',
+  output: 'static',
 });
